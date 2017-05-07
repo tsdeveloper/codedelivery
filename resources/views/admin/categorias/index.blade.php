a49f881b51c44a102f9630adb2bc4889436c50f8
Thiago Almeida <ts.analista@gmail.com>
2017-05-07 19:49:20 -0300

    Add Listagem da Categoria na View

---
+++ resources/views/admin/categorias/index.blade.php
@@ -0,0 +1,42 @@
+    @extends('app')
+        @section('content')
+<div class="container">
+            <div class="content"> 
+
+            <br/>
+<span class="col-md-4 control-label">Seja bem Vindo <strong>{{$usuario}}</strong></span>
+<br />
+
+<div class="title"><h3>Área Administrativa de Categorias</h3></div>
+
+<br /> 
+<a href="#" class="btn btn-default">Nova Categoria</a>
+<br /> 
+<br /> 
+<table class="table table-striped">
+    <thead>
+   
+        <tr>
+            <th>ID</th>
+            <th>Descrição</th>
+            <th>Ação</th>
+        </tr>
+    </thead>
+    <tbody>
+     @foreach($categorias as $categoria)
+        <tr>
+            <td>{{$categoria->id}}</td>
+            <td>{{$categoria->descricao}}</td>
+            <td>
+            <a href="#" class="btn btn-info">Editar</a>
+            <a href="#" class="btn btn-danger">Excluir</a>
+            </td>
+        </tr>
+        @endforeach 
+    </tbody>
+</table>
+
+
+</div>
+</div>
+@endsection
