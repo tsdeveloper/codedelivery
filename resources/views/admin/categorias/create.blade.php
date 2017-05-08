73f53837610dcd8904647a688245088bff534173
Thiago Almeida <ts.analista@gmail.com>
2017-05-07 21:30:45 -0300

    Add title  create

--- resources/views/admin/categorias/create.blade.php
+++ resources/views/admin/categorias/create.blade.php
@@ -1,4 +1,12 @@
     @extends('app')
         @section('content')
+<div class="container">
+            <div class="content"> 
 
-        @endsection
+<div class="title"><h3>Nova Categorias</h3></div>
+</div>
+<br /> 
+
+<a href=" {{ route('admin.categorias.index') }} " class="btn btn-default">Voltar</a>
+</div>
+@endsection
