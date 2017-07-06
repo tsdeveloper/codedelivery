47d1e452b7d2ecdc866ebcbd503cdac3e73c71bd
Thiago Almeida <ts.analista@gmail.com>
2017-07-05 22:41:04 -0300

    Add lib Colllective HTML

--- config/app.php
+++ config/app.php
@@ -139,7 +139,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        // Illuminate\Html\HtmlServiceProvider::class,
+         Collective\Html\HtmlServiceProvider::class,
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         Bestmomo\Scafold\ScafoldServiceProvider::class,
         /*
@@ -199,8 +200,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        "Html"      => Illuminate\Html\HtmlFacade::class,
-        "Form"      => Illuminate\Html\FormFacade::class,
+        // "Html"      => Illuminate\Html\HtmlFacade::class,
+        // "Form"      => Illuminate\Html\FormFacade::class,
+          'Form' => Collective\Html\FormFacade::class,
+      'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
