7e4a986a987cac49b3cba6a0651f31206c634800
Thiago Almeida <ts.analista@gmail.com>
2017-07-06 23:15:23 -0300

    Include check error blade

--- resources/views/admin/clients/create.blade.php
+++ resources/views/admin/clients/create.blade.php
@@ -3,8 +3,9 @@
 <div class="container">
             <div class="content"> 
 
-<div class="title"><h3>Novo Cliente</h3></div>
+<div class="title"><h3>Novo Pedido</h3></div>
 </div>
+   @include('errors._check')
 <br /> 
     {!! Form::open(['route'=>'admin.clients.store']) !!}
 
@@ -17,7 +18,8 @@
         </ul>
     </div>
     @endif --}}
-    @if (count($errors) > 0)
+ 
+    {{-- @if (count($errors) > 0)
     <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
@@ -25,13 +27,45 @@
             @endforeach
         </ul>
     </div>
-@endif
-   
-   @include('admin.clients._form')
+@endif --}}
 
  <div class="form-group">
-{!! Form::submit('Criar Cliente',['class'=>'btn btn-primary']) !!}
+    <label>Total: </label>
+    <p id="total"></p>
+   {!! Form::submit('Novo Item',['class'=>'btn btn-primary']) !!}
+
+   <div class="table-responsive">
+
+<table class="table table-striped table-hover">
+    <thead>
+   
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Produto</th>
+            <th>Status</th>
+            <th>Ação</th>
+        </tr>
+    </thead>
+    <tbody>
+     @foreach($clients as $client)
+        <tr>
+            <td>{{$client->id}}</td>
+            <td>{{$client->user->name}}</td>
+            <td>{{$client->user->email}}</td>
+            <td>{{ $client->active=== 1 ? 'Ativo' : 'Inativo' }}</td>
+            <td>
+            <a href="{{ route('admin.clients.edit',['id'=>$client->id]) }}" class="btn btn-info">Editar</a>
+            <a href="#" class="btn btn-danger">Excluir</a>
+            </td>
+        </tr>
+        @endforeach 
+    </tbody>
+</table>
+
  </div>
+
+
     {!! Form::close() !!}
  <div class="form-group">
 <a href=" {{ route('admin.clients.index') }} " class="btn btn-default">Voltar</a>    
