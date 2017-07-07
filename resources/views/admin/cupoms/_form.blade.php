5437eb40c1c0de83499b71df02e9d6d3a858ac44
Thiago Almeida <ts.analista@gmail.com>
2017-07-06 21:48:54 -0300

    Add csrf token view cupom

--- resources/views/admin/cupoms/_form.blade.php
+++ resources/views/admin/cupoms/_form.blade.php
@@ -4,48 +4,38 @@
      {!! Form::select('user_id', $users, null, ['class'=>'form-control']) !!}      
     </div> --}}
 <section id='section-register'>
-    <h3>Dados de Cadastro</h3>
+    <h3>Dados do Cupom</h3>
      <div class="form-group">
-     {!! Form::label('Name','Nome Cliente',['class'=>'col-md-2']) !!}
-     {!! Form::text('user[name]',null,['class'=>'form-control']) !!}      
+     {!! Form::label('Description','Descrição',['class'=>'label-control col-md-2']) !!}
+     <div class="col-md-4">
+     {!! Form::text('description',null,['class'=>'form-control']) !!}      
+     </div>
     </div>
     
     <div class="form-group">
-     {!! Form::label('Address','Endereço',['class'=>'col-md-2']) !!}
-     {!! Form::text('address',null,['class'=>'form-control']) !!}      
+     {!! Form::label('Code','Cupom',['class'=>'label-control col-md-2']) !!}
+     <div class="col-md-2">
+     {!! Form::text('code',null,['class'=>'form-control']) !!} 
+      
+    </div>
+    {!! Form::button('Gerar Cupom',['class'=>'btn btn-success','id'=>'btn-code']) !!}
     </div>
 
     <div class="form-group">
-     {!! Form::label('Zipcode','Cep',['class'=>'col-md-2']) !!}
-     {!! Form::text('zipcode',null,['class'=>'form-control']) !!}      
+     {!! Form::label('Validate','Validade',['class'=>'label-control col-md-2']) !!}
+      <div class="col-md-2">
+     {!! Form::date('validate',null,['class'=>'form-control']) !!}      
+    </div>
     </div>
 
     <div class="form-group">
-     {!! Form::label('Phone','Telefone',['class'=>'col-md-2']) !!}
-     {!! Form::text('phone',null,['class'=>'form-control']) !!}      
+     {!! Form::label('Value','Valor',['class'=>'label-control col-md-2']) !!}
+      <div class="col-md-4">
+     {!! Form::text('value',null,['class'=>'form-control']) !!}      
     </div>
-
-    <div class="form-group">
-     {!! Form::label('City','Cidade',['class'=>'col-md-2']) !!}
-     {!! Form::text('city',null,['class'=>'form-control']) !!}      
     </div>
-
-    <div class="form-group">
-     {!! Form::label('State','UF',['class'=>'col-md-2']) !!}
-     {!! Form::text('state',null,['class'=>'form-control']) !!}      
-    </div>
+   
+   
     </section>
 
-    <section id='section-access'>
-        <h3>Dados de acesso</h3>
-    <div class="form-group">
-     {!! Form::label('User[email]','Login',['class'=>'col-md-2']) !!}
-     {!! Form::email('user[email]',null,['class'=>'form-control']) !!}      
-    </div>
-{{-- 
-    <div class="form-group">
-     {!! Form::label('User[password]','Senha',['class'=>'col-md-2']) !!}
-     {!! Form::text('user[password]',null,['class'=>'form-control']) !!}      
-    </div> --}}
-
-    </section>
+    
