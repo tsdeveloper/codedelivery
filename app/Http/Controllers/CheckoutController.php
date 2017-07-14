cccd74c45042c2420291ec1e673f1f21de736ebf
Thiago Almeida <ts.analista@gmail.com>
2017-07-14 20:58:59 -0300

    Add Order Service Checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace CodeDelivery\Http\Controllers;
 
 
+use CodeDelivery\Services\OrderService;
 use Illuminate\Http\Request;
 use CodeDelivery\Http\Requests;
 use CodeDelivery\Http\Controllers\Controller;
@@ -12,6 +13,7 @@ use Prettus\Validator\Contracts\ValidatorInterface;
 use CodeDelivery\Repositories\UserRepository;
 use CodeDelivery\Repositories\OrderRepository;
 use CodeDelivery\Repositories\ProductRepository;
+use function redirect;
 
 class CheckoutController extends Controller
 {
@@ -19,20 +21,35 @@ class CheckoutController extends Controller
     private $_userRepository;
     private $_orderRepository;
     private $_productRepository;
+    /**
+     * @var OrderService
+     */
+    private $service;
 
     public function __construct(
         UserRepository $userRepository,
         OrderRepository $orderRepository,
         ProductRepository $productRepository,
-        OrderService service) {
+        OrderService $service) {
         $this->_userRepository=$userRepository;
         $this->_orderRepository=$orderRepository;
         $this->_productRepository=$productRepository;
+        $this->service = $service;
+    }
+
+    public function  index(){
+        $clientId = $this->_userRepository->find(Auth::user()->id)->client->id;
+        $orders = $this->_orderRepository->scopeQuery(function ($query) use($clientId){
+           return $query->where('client_id', '=', $clientId);
+        })-paginate(5);
+
+        return view('customer.order.index',compact( '$orders'));
+
     }
 
     public function create(){
 
-        $products = $this->_productRepository->lists();
+            $products = $this->_productRepository->lists();
 
         return view('customer.order.create',compact( 'products'));
     }
@@ -45,7 +62,7 @@ class CheckoutController extends Controller
         $data['client_id'] = $clientId;
         $this->service->create($data);
 
-        return view('customer.order.create',compact('products'));
+        return redirect()->route('customer.order.index');
     }
      
 }
