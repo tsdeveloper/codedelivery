5e39debf75fa0c844fa1c865782619b5912afb05
Thiago Almeida <ts.analista@gmail.com>
2017-07-15 00:01:59 -0300

    Add new file bug Laragon

---
+++ resources/views/customer/order/index.blade.php
@@ -0,0 +1,34 @@
+<?php
+
+@extends('app')
+@section('content')
+    <div class="container">
+        <h3>Meus Pedidos</h3>
+        <a href="{{route('customer.order.create')}}" class="btn btn-default">Novo Pedido</a>
+        <br><br>
+
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                
+                <tbody>
+                @foreach($orders as $order)
+                <tr>
+                    <td>{{$order->id}}</td>
+                    <td>{{$order->total}}</td>
+                    <td>{{$order->status}}</td>
+
+                </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        {!! $orders->render() !!}
+    </div>
