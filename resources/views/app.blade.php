9ff47f1a95b5720e98b7c60900dacd7e30c27560
Thiago Almeida <ts.analista@gmail.com>
2017-07-19 20:20:28 -0300

    Add Create View Customer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	{{--<meta name="csrf-token" content="{{ csrf_token() }}">--}}
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Laravel</title>
 
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
