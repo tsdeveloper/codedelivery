976f8965c0bdc49803a48a3bec4ae16ab8b8e2c7
Thiago Almeida <ts.analista@gmail.com>
2017-08-21 20:18:54 -0300

    Add Controller Angular-ui-Route

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,22 +3,50 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home/:name?',
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+            })
+            .state('home.a', {
+                url: '/a',
+                templateUrl: 'templates/home-a.html'
+            })
+            // .state('home.a.childA', {
+            //     url: '/childA',
+            //     templateUrl: 'templates/main-a.html'
+            // })
+
+            .state('home.b', {
+                url: '/b',
+                templateUrl: 'templates/home-b.html'
+            })
+            .state('main', {
+                url: '/main',
+                templateUrl: 'templates/main.html'
+            });
+
+        $urlRouterProvider.otherwise('/')
+    });
